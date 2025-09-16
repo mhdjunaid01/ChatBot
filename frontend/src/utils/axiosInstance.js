36ad3fd3e63382886b86b8c608286be595d4353d
Muhammed junaid m <108187745+mhdjunaid01@users.noreply.github.com>
2025-09-17 02:38:24 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/", 
-  baseURL: `${import.meta.env.VITE_API_URL}`, 
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, 
 });
 
